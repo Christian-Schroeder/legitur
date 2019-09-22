df29f7c797cf5197e1462a98666f78c4920275da
Christian Schroeder <c.schroeder@alta-schwede.de>
2019-09-22 13:07:02 +0200

    make it start without error

--- app/src/main/java/de/altaschwede/legitur/MyAdapter.kt
+++ app/src/main/java/de/altaschwede/legitur/MyAdapter.kt
@@ -1,10 +1,11 @@
 package de.altaschwede.legitur
 
-import alta_schwede.de.legitur.R
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import alta_schwede.de.legitur.R
 
 class MyAdapter(private val myDataset: Array<String>) :
         RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
@@ -13,8 +14,20 @@ class MyAdapter(private val myDataset: Array<String>) :
     // Complex data items may need more than one view per item, and
     // you provide access to all the views for a data item in a view holder.
     // Each data item is just a string in this case that is shown in a TextView.
-    class MyViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
+    //class MyViewHolder(val view: View) : RecyclerView.ViewHolder(view)
+    // stores and recycles views as they are scrolled off screen
+    inner class MyViewHolder internal constructor(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
+        internal var myTextView: TextView
 
+        init {
+            myTextView = itemView.findViewById(R.id.bookName)
+            itemView.setOnClickListener(this)
+        }
+
+        override fun onClick(view: View) {
+            //if (mClickListener != null) mClickListener.onItemClick(view, adapterPosition)
+        }
+    }
 
     // Create new views (invoked by the layout manager)
     override fun onCreateViewHolder(parent: ViewGroup,
@@ -23,7 +36,7 @@ class MyAdapter(private val myDataset: Array<String>) :
 
         // create a new view
         val textView = LayoutInflater.from(parent.context)
-                .inflate(R.layout.row, parent, false) as TextView
+                .inflate(R.layout.row, parent, false)
         // set the view's size, margins, paddings and layout parameters
 
         return MyViewHolder(textView)
@@ -33,7 +46,7 @@ class MyAdapter(private val myDataset: Array<String>) :
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         // - get element from your dataset at this position
         // - replace the contents of the view with that element
-        holder.textView.text = myDataset[position]
+        holder.myTextView.text = myDataset[position]
     }
 
     // Return the size of your dataset (invoked by the layout manager)
