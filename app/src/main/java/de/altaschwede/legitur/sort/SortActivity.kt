785f6d81b1aa40ae31516ee23ad5b69917cbb999
Christian <c.schroeder@alta-schwede.de>
2020-04-21 20:45:31 +0200

    implement first partly working variant of sort

--- app/src/main/java/de/altaschwede/legitur/sort/SortActivity.kt
+++ app/src/main/java/de/altaschwede/legitur/sort/SortActivity.kt
@@ -1,15 +1,17 @@
 package de.altaschwede.legitur.sort
 
 import android.os.Bundle
-import android.view.View
-import android.widget.EditText
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import de.altaschwede.legitur.R
 import de.altaschwede.legitur.books.BooksViewModel
 import de.altaschwede.legitur.books.db.Book
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
 
 
 class SortActivity : AppCompatActivity() {
@@ -23,22 +25,46 @@ class SortActivity : AppCompatActivity() {
         model = ViewModelProvider(this, ViewModelProvider.AndroidViewModelFactory
                 .getInstance(this.application)).get(BooksViewModel::class.java)
 
-        GlobalScope.launch { startSorting() }
+        val async = GlobalScope.async {
+            model.getAllPostsDirect()
+        }
+        GlobalScope.launch {
+            sort(async.await())
+        }
     }
 
-    private suspend fun startSorting() {
-        val allPosts = model.getAllPostsDirect()
-        for (b: Book in allPosts!!) {
-            println(b.title)
+    private suspend fun sort(books: List<Book>?) {
+        if (books != null) {
+            for ((n, _) in books.withIndex()) {
+                for (i in n - 1 downTo 0) {
+                    val o1 = books[i]
+                    val o2 = books[i + 1]
+                    if (compareBooks(o1, o2) == 1) {
+                        //A.swap(i, i+1)
+                        saveBook(o1.copy(position = o1.position + 1))
+                        saveBook(o2.copy(position = o2.position - 1))
+                    } // Ende if
+                }
+            }
+        }
+    }
+
+    private suspend fun compareBooks(o1: Book, o2: Book): Int {
+        return suspendCoroutine { continuation ->
+            val first = findViewById<Button>(R.id.textView)
+            val second = findViewById<Button>(R.id.textView2)
+            runOnUiThread {
+                first.text = o1.title
+                second.text = o2.title
+            }
+            first.setOnClickListener { continuation.resume(1) }
+            second.setOnClickListener { continuation.resume(-1) }
         }
     }
 
 
-    fun saveBook(@Suppress("UNUSED_PARAMETER") view: View) {
-        val editText = findViewById<EditText>(R.id.title)
-        val message = editText.text.toString()
-        val book = Book(null, message)
-
+    private fun saveBook(book: Book) {
+        println(book)
         model.savePost(book)
     }
 
